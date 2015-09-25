51bfbc0a8a7af9afffabda5dc0aff110d798cfed
david <david@david-pc>
2015-09-25 23:20:45 +0400

    Author David   -Add favicon.

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -16,6 +16,7 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
 
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
+	<link rel="shortcut icon" href="images/favicon.png" />
 </head>
 
 <body>
